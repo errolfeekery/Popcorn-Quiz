22fbda11cc47fd330618a64de0b76e055bd83520
errolfeekery <errolfeekery@gmail.com>
2022-10-01 13:53:05 +0000

    add first questions to javascript

---
+++ media/myScript.js
@@ -0,0 +1,10 @@
+const Questions = [
+    {
+        id: 0,
+        question: "The code in The Martrix comes from what food recipes?",
+        answer: [{text: "Sushi recipes", isCorrect: true},
+                 {text: "Dumpling recipes", isCorrect: false},
+                 {text: "Stir-fry recipes", isCorrect: false},
+                 {text: "Pad thai recpies", isCorrect: false}
+    ]
+    }]
