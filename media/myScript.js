1e9650855b7adfd98d1e11780d27e9371bb37dbd
errolfeekery <errolfeekery@gmail.com>
2022-10-01 14:14:29 +0000

    add more questions

--- media/myScript.js
+++ media/myScript.js
@@ -6,5 +6,51 @@ const Questions = [
                  {text: "Dumpling recipes", isCorrect: false},
                  {text: "Stir-fry recipes", isCorrect: false},
                  {text: "Pad thai recpies", isCorrect: false}
-    ]
-    }]
+        ]
+    },
+    {
+        id: 1,
+        question: "Where were The Lord of the Rings movies filmed?",
+        answer: [{text: "Ireland", isCorrect: false},
+                 {text: "Iceland", isCorrect: false},
+                 {text: "New Zealand", isCorrect: true},
+                 {text: "Australia", isCorrect: false}
+        ]
+    },
+    {
+        id: 2,
+        question: "If you were to watch the Marvel movies in chronological order, which movie would you watch first?",
+        answer: [{text: "Iron man", isCorrect: false},
+                 {text: "Captain America: The First Avenger", isCorrect: true},
+                 {text: "Doctor Strange", isCorrect: false},
+                 {text: "Thor", isCorrect: false}
+        ]
+    },
+    {
+        id: 3,
+        question: "Which movie is this quote from: 'Here's looking at you kid'",
+        answer: [{text: "Breakfast at Tiffany's", isCorrect: false},
+                 {text: "Citizen Kane", isCorrect: false},
+                 {text: "Casablanca", isCorrect: true},
+                 {text: "Notorious", isCorrect: false}
+        ]
+    },
+    {
+        id: 4,
+        question: "Which famous line from Jaws was ad-libbed?",
+        answer: [{text: "'I use to hate the water'", isCorrect: false},
+                 {text: "'Your gonna need a bigger boat'", isCorrect: true},
+                 {text: "'All this machine does is swim, and eat, and make little sharks'", isCorrect: false},
+                 {text: "'You know, a thing about a shark, hes got lifeless eyes. Black eyes, like a dolls eyes'", isCorrect: false}
+        ]
+    },
+    {
+        id: 5,
+        question: "Who played Wendy Torrence in The Shining?",
+        answer: [{text: "Cher", isCorrect: false},
+                 {text: "Sissy Spacek", isCorrect: false},
+                 {text: "Shelley Duvall", isCorrect: true},
+                 {text: "Linda Ronstadt", isCorrect: false}
+        ]
+    },
+]
